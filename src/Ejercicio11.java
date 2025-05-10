67b7e81a8f6181cca1bb9854e1b5c4254a0609d2
412366-Marucco <412366@tecnicatura.frc.utn.edu.ar>
2025-05-10 17:54:03 -0300

    Exercise 11 Done.

---
+++ src/Ejercicio11.java
@@ -0,0 +1,27 @@
+//11.Ingresar el tiempo en segundos que realizó el ganador de la clasificación de una
+//carrera de F1. Luego ingresar los tiempos de los otros 9 corredores e informar
+//cuántos disputarán la carrera. Para que un corredor pueda participar su tiempo de
+//clasificación no puede superar en 15 % al del ganador.
+
+import java.util.Scanner;
+
+public class Ejercicio11 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingresar el tiempo del ganador de la carrera: ");
+        double tiempoGanador = scanner.nextDouble();
+
+        int cuantosCorren = 1;
+        System.out.println("Ingresar el tiempo de los otros 9 corredores: ");
+        for (int i = 0; i < 9; i++) {
+
+            System.out.println("Ingresar el tiempo: ");
+            double tiempoCorredor = scanner.nextDouble();
+            if (tiempoCorredor <= tiempoGanador * 1.15) {
+                cuantosCorren++;
+            }
+        }
+        System.out.println("Correran " + cuantosCorren + " corredores.");
+    }
+}
